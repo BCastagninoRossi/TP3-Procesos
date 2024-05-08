5df810fee04657c5b693ed19e55b9412b99670c6
Bruno Castagnino Rossi <87082300+BCastagninoRossi@users.noreply.github.com>
2024-05-07 23:59:14 -0300

    listo

--- src/ej2/shell.c
+++ src/ej2/shell.c
@@ -5,15 +5,13 @@
 #include <string.h>
 
 #define MAX_COMMANDS 200
-
 int main() {
 
     char command[256];
     char *commands[MAX_COMMANDS];
     int command_count = 0;
 
-    while (1) 
-    {
+    while (1) {
         printf("Shell> ");
         
         /*Reads a line of input from the user from the standard input (stdin) and stores it in the variable command */
@@ -30,77 +28,83 @@ int main() {
            The strtok() function breaks the command string into tokens (substrings) separated by the pipe character |. 
            In each iteration of the while loop, strtok() returns the next token found in command. 
            The tokens are stored in the commands[] array, and command_count is incremented to keep track of the number of tokens found. */
-        char *token = strtok(command, "|");
+        char *token = strtok(command, "|"); //ls | grep ".zip"
         while (token != NULL) 
         {
             commands[command_count++] = token;
             token = strtok(NULL, "|");
         }
+        
+        // int *PIDS = malloc(sizeof(int) * command_count);
+        // if(!PIDS){return -1;}
 
-        /* You should start programming from here... */
-        int PID[command_count];
-
-        char * command_list [command_count][MAX_COMMANDS];
-        for (int i = 0 ; i < command_count ; i++){
-            char* token = strtok(commands[i], " ");
-            int j = 0;
-            while (token != NULL){
-                if (token[0] == '"' && token[strlen(token)-1]=='"'){
-                    token++;
-                    token[strlen(token)-1] = '\0';
-                }
-                command_list[i][j++] = token;
-                token = strtok(NULL, " ");
-            }
-            command_list[i][j] = NULL;
-        }
-
-        if (command_count == 1){
-            execvp(command_list[0][0], command_list[0]);
-            return 0;
-        }
-
-        int PIPES[command_count-1][2];
-        for (int i = 0; i < (command_count-1); i++){
-            if (pipe(PIPES[i])==-1){
-                fprintf(stderr, "Error while creating pipes\n");
-            }
-        }
-
-        for (int i = 0; i < command_count; i++) 
-        {
-            printf("Command %d: %s\n", i, commands[i]);
-            switch (PID[i]==fork())
-            {
-            case -1:
-                fprintf(stderr, "Error initializing fork()\n");
+        int PIDS[command_count];
+        
+        // int **pipes = create_pipes(command_count - 1);
+        int pipes[command_count - 1][2];
+        for (int i = 0; i < command_count - 1; i++){
+            if (pipe(pipes[i]) == -1){
+                fprintf(stderr, "Error al crear los pipes\n");
                 return -1;
-
-            case 0:
-                if (i !=0){
-                    if (dup2(PIPES[i-1][0], STDIN_FILENO) == -1){
-                        return -1;
-                    }
-                    close(PIPES[i-1][0]);
-                }
-                if (i != command_count-1){
-                    if (dup2(PIPES[i][0], STDOUT_FILENO) == -1){
-                    return -1;
-                    }
-                    close(PIPES[i][1]);
-                }
-                for (int j = 0; j < command_count; j++){
-                    close(PIPES[j][0]);
-                    close(PIPES[j][1]);
-                }
-                execvp(command_list[i][0], command_list[i]);
-                perror("execvp");
-                return 0;
-            default:
-                wait(NULL);
             }
         }
-        command_count = 0;
+        /* You should start programming from here... */
+        char * command_matrix [command_count][MAX_COMMANDS];
+        for (int i = 0; i < command_count; i++) {
+            char *arg = strtok(commands[i], " ");
+            int j = 0;
+            while (arg != NULL) {
+                if (arg[0] == '"' && arg[strlen(arg) - 1] == '"') {
+                    arg++;
+                    arg[strlen(arg) - 1] = '\0';
+                }
+                command_matrix[i][j++] = arg;
+                arg = strtok(NULL, " ");
+            }
+            command_matrix[i][j] = NULL; 
+        }
+
+        for (int i = 0; i < command_count; i++) {
+            printf("Comando %d: %s\n", i, commands[i]);
+            switch (PIDS[i] = fork()) {
+                case -1:
+                    fprintf(stderr, "Error al hacer el fork()\n");
+                    return -1;
+                    exit(1);
+
+                case 0:
+                    if (i != 0) {
+                        close(pipes[i - 1][1]);
+                        if(dup2(pipes[i - 1][0], STDIN_FILENO) == -1){
+                            return -1;
+                        }
+                        close(pipes[i - 1][0]);
+                    }
+                    if (i != command_count - 1) {
+                        close(pipes[i][0]);
+                        if(dup2(pipes[i][1], STDOUT_FILENO)  == -1){
+                            return -1;
+                        }
+                        close(pipes[i][1]);
+                    }
+                    for (int j = 0; j < command_count -1; j++) {
+                        close(pipes[j][0]);
+                        close(pipes[j][1]);
+                    }
+                    execvp(command_matrix[i][0], command_matrix[i]);
+                    perror("execvp");
+                    exit(0);
+                default:
+                    wait(NULL);
+                    if (i != 0) {
+                        close(pipes[i - 1][0]); // si no se queda esperando
+                    }
+                    if (i != command_count - 1) {
+                        close(pipes[i][1]);
+                    }
+            }
+        }
+        command_count = 0; 
     }
     return 0;
 }
